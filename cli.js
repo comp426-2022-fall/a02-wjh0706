89a1d4a7129019547a9e67833bc62baba52b565e
wjh0706 <wujianghao0706@gmail.com>
2022-10-06 15:50:00 -0400

    v1.00

--- cli.js
+++ cli.js
@@ -7,37 +7,36 @@ import moment from "moment-timezone";
 const args = minimist(process.argv.slice(2))
 
 if(args.h){
-  	console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+  	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
     -h            Show this help message and exit.
     -n, -s        Latitude: N positive; S negative.
     -e, -w        Longitude: E positive; W negative.
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.');
+    -j            Echo pretty JSON from open-meteo API and exit.`);
 	process.exit(0);
 }
 
-const timezone = moment.tz.guest();
+var timezone = moment.tz.guess();
 if(args.t){
     timezone = args.t;
 }
-
-const latitude;
-const longitude;
+var latitude;
+var longitude;
 if(args.n!=null){
-	latitude = args.n;
+  latitude = args.n;
 }else if (args.s!=null){
-	latitude = args.s;
+  latitude = args.s*-1;
 }else{
-	latitude = 0;
+  latitude = 0;
 }
 
 if(args.e!=null){
-	longitude = args.e;
+  longitude = args.e;
 }else if (args.w!=null){
-	longitude = args.w;
+  longitude = args.w*-1;
 }else{
-	longitude = 0;
+  longitude = 0;
 }
 	
 
